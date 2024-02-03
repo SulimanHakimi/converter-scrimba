827e83bd12e0f8dddc5b65f17b469310286e12f4
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-02-03 01:47:23 -0800

    fix numbers decimal to 3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let kiloOutput = document.getElementById("kilo-output");
 let literOutput = document.getElementById("liter-output");
 
 convertBtn.addEventListener("click", () => {
-  meterOutput.innerHTML = `${input.value} meters = ${input.value * 3.28084} feet | ${input.value} feet = ${input.value / 3.28084} meters`
-  kiloOutput.innerHTML = `${input.value} liter = ${input.value * 0.26} gallons | ${input.value} gallons = ${input.value /0.26} liter`
-  literOutput.innerHTML = ` ${input.value} kilos = ${input.value * 2.20462} pounds | ${input.value} pounds = ${input.value / 0.453592} kilos`
+  meterOutput.innerHTML = `${input.value} meters = ${(input.value * 3.28084).toFixed(3)} feet | ${input.value} feet = ${(input.value / 3.28084).toFixed(3)} meters`
+  kiloOutput.innerHTML = `${input.value} liter = ${(input.value * 0.26).toFixed(3)} gallons | ${input.value} gallons = ${(input.value /0.26).toFixed(3)} liter`
+  literOutput.innerHTML = ` ${input.value} kilos = ${(input.value * 2.20462).toFixed(3)} pounds | ${input.value} pounds = ${(input.value / 0.453592).toFixed(3)} kilos`
   });
